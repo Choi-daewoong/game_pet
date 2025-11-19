0b9ae5afbe4a04a7e45a7fb41e58be5eb1836449
Choi-daewoong <Choi-daewoong@users.noreply.github.com>
2025-11-19 13:50:59 +0000

    deploy: b9826f07104bbde001d9af130c5731de0e2cdb92

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "41295456"
+    serviceWorkerVersion: "2652137274"
   }
 });
