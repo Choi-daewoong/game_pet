9d43740fb4965a7cdf6fbcdc81e318f82ebd515e
Choi-daewoong <Choi-daewoong@users.noreply.github.com>
2025-11-19 13:43:28 +0000

    deploy: 69462ac0bcf23c0279b1e2480a98287cf57a59cd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "manifest.json": "314a2dc45aa0a9fb0f6e323c155c44c2",
-"index.html": "d5110929bd73ae7a1a4f9f9b8c7aa78e",
-"/": "d5110929bd73ae7a1a4f9f9b8c7aa78e",
+"index.html": "ac848d50f9b91914cb135aa5f9f4c23e",
+"/": "ac848d50f9b91914cb135aa5f9f4c23e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/fonts/MaterialIcons-Regular.otf": "d0159028da61b2cf17e071300a32d06b",
@@ -26,7 +26,7 @@ const RESOURCES = {"flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
-"flutter_bootstrap.js": "1a60ace9c11a15973244a4bbd87a9479",
+"flutter_bootstrap.js": "5d5eaa8b27ceac73606d623f31b9f252",
 "version.json": "b85ef9d96e133c9c09b1e93db8397409",
 "main.dart.js": "34bf8d5c8280b8f9fc8437add4b64425"};
 // The application shell files that are downloaded before a service worker can
